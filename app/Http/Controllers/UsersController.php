c47de171a90d753495d5ff4d5208bad1699e06a8
duoduola0 <853988880@139.com>
2019-08-22 07:41:24 +0000

    编辑个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 
 class UsersController extends Controller
@@ -11,4 +13,17 @@ class UsersController extends Controller
     {
         return view('users.show', compact('user'));
     }
+
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(UserRequest $request, User $user)
+    {
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
+    }
+
+    
 }
